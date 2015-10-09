a0c5e6b5df04830cb4e662dcbb65e3855a2fb053
ypcrts <ypcrts@users.noreply.github.com>
2015-10-09 10:04:49 -0400

    add cleanup to vertex theme script

--- x-environment-gtk-theme.bash
+++ x-environment-gtk-theme.bash
@@ -11,16 +11,19 @@ sudo rm -rf /usr/share/themes/{Vertex,Vertex-Dark,Vertex-Light,Vertex-Gnome-Shel
 rm -rf ~/.local/share/themes/{Vertex,Vertex-Dark,Vertex-Light,Vertex-Gnome-Shell,Vertex-Gnome-Shell-3.16,Vertex-Cinnamon}
 rm -rf ~/.themes/{Vertex,Vertex-Dark,Vertex-Light,Vertex-Gnome-Shell,Vertex-Gnome-Shell-3.16,Vertex-Cinnamon}
 
+rm -rf vertex-theme
 git clone https://github.com/horst3180/vertex-theme --depth 1 
+
 cd vertex-theme
 git fetch --tags
 git checkout $(git describe --tags `git rev-list --tags --max-count=1`)
 
 ./autogen.sh --prefix=/usr \
   --disable-cinnamon \
-  --disable-gnome-shell \
   --disable-metacity \
   --disable-unity \
   --disable-xfwm 
 
 sudo make install
+cd ..
+rm -rf vertex-theme
