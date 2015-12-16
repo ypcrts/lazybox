dc92816c7c6b935d73b7ea4edf960b6d1fb2c32f
ypcrts <ypcrts@users.noreply.github.com>
2015-12-16 00:17:01 -0500

    lemonbar betterment

---
+++ x-environment-lemonbar.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -xe
+
+git clone git://github.com/lemonboy/bar ./lemonbar && cd ./lemonbar || cd ./lemonbar && git pull origin 
+
+make all
+
+echo "make install? [y/N]" 
+read n
+case "$n" in 
+  "y"|"Y"|"J")
+    ;;
+  *)
+    exit 1
+    ;;
+esac
+
+sudo install -dm 655 /usr/local/stow/lemonbar
+sudo make PREFIX=/ DESTDIR=/usr/local/stow/lemonbar install
+sudo stow -d /usr/local/stow -S lemonbar
