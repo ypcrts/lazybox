d37ebfa18602115b1aba3b55c0758c68a16d02b8
ypcrts <ypcrts@users.noreply.github.com>
2016-10-31 14:12:20 -0400

    with-default-names was failing

--- darwin/homebrew-basic-utils.sh
+++ darwin/homebrew-basic-utils.sh
@@ -1,2 +1,2 @@
-brew install --with-default-names coreutils binutils diffutils ed findutils gawk gnu-indent gnu-sed gnu-tar gnu-which gnutls grep gzip tmux zsh bash vim git
+brew install coreutils binutils diffutils ed findutils gawk gnu-indent gnu-sed gnu-tar gnu-which gnutls grep gzip tmux zsh bash vim git --with-default-names
 brew install wdiff  --with-gettext
