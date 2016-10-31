f321d251e81c25b65ab0bb9bb8ba4ef7e7962672
ypcrts <ypcrts@users.noreply.github.com>
2016-10-31 14:39:28 -0400

    more brews

--- darwin/homebrew-basic-utils.sh
+++ darwin/homebrew-basic-utils.sh
@@ -1,2 +1,5 @@
+brew tap homebrew/dupes
 brew install coreutils binutils diffutils ed findutils gawk gnu-indent gnu-sed gnu-tar gnu-which gnutls grep gzip tmux zsh bash vim git --with-default-names
-brew install wdiff  --with-gettext
+brew install wdiff --with-gettext
+brew install python python3
+brew install bash-completion zsh-completiions
