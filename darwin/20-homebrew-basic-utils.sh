e7aa698e72afb3d8161d96580c9c98abab3df330
ypcrts <ypcrts@users.noreply.github.com>
2016-12-07 15:25:55 -0500

    more shit?

--- darwin/20-homebrew-basic-utils.sh
+++ darwin/20-homebrew-basic-utils.sh
@@ -1,7 +1,8 @@
 brew tap homebrew/dupes
-brew install coreutils binutils diffutils ed findutils gawk gnu-indent gnu-sed gnu-tar gnu-which gnutls grep gzip tmux zsh bash vim git rename --with-default-names
+brew install vim  --with-lua  --with-override-system-vi
+brew install coreutils binutils diffutils ed findutils gawk gnu-indent gnu-sed gnu-tar gnu-which gnutls grep gzip tmux zsh bash git rename --with-default-names
 brew install wdiff colordiff --with-gettext
 brew install python python3 pyenv-virutalenvwrapper
 brew install bash-completion zsh-completiions
-brew install tree ag 
-
+brew install tree ag watch
+brew install https://raw.githubusercontent.com/kadwanev/bigboybrew/a642410aaf459184ce1ff9f2a0498b60a9106a1d/Library/Formula/sshpass.rb
