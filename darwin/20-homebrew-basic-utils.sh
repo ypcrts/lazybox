0bb205ea87cf3ecb9149930545cf521cf9578aff
ypcrts <ypcrts@users.noreply.github.com>
2016-12-07 22:40:20 -0500

    nvim wins

--- darwin/20-homebrew-basic-utils.sh
+++ darwin/20-homebrew-basic-utils.sh
@@ -6,3 +6,4 @@ brew install python python3 pyenv-virutalenvwrapper
 brew install bash-completion zsh-completiions
 brew install tree ag watch
 brew install https://raw.githubusercontent.com/kadwanev/bigboybrew/a642410aaf459184ce1ff9f2a0498b60a9106a1d/Library/Formula/sshpass.rb
+brew install neovim/neovim/neovim
