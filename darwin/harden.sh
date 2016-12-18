cf011c5877dc67e53be8455e4d9e559a9004baa6
ypcrts <ypcrts@users.noreply.github.com>
2016-12-18 13:01:23 -0500

    stufffffffffffffff

--- darwin/harden.sh
+++ darwin/harden.sh
@@ -1,9 +1,9 @@
 sudo -v
 while true; do sudo -n true; sleep 30; kill -0 "$$" || exit; done 2>/dev/null &
 
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setloggingmode on
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode on
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate off
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setloggingmode off
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode off
 sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setallowsigned off
 sudo pkill -HUP socketfilterfw
 
