6759278d43a1489b1d49a7637fd66bfb00df838c
ypcrts <ypcrts@users.noreply.github.com>
2017-05-29 16:56:24 -0400

    what i wouldn't give for apt on macos

--- darwin/20-homebrew-basic-utils.sh
+++ darwin/20-homebrew-basic-utils.sh
@@ -1,8 +1,8 @@
 brew tap homebrew/dupes
 brew install vim  --with-lua  --with-override-system-vi
-brew install coreutils binutils diffutils ed findutils gawk gnu-indent gnu-sed gnu-tar gnu-which gnutls grep gzip tmux zsh bash git rename --with-default-names
-brew install neovim/neovim/neovim
-brew install bash-completion zsh-completiions
+brew install coreutils binutils diffutils gnupg pgpdump ed findutils gawk gnu-indent gnu-sed gnu-tar gnu-which gnutls grep gzip tmux zsh bash git rename --with-default-names
+# brew install neovim/neovim/neovim  # compile it yourself because you love that
+brew install bash-completion zsh-completions
 brew install wdiff colordiff --with-gettext
 brew install python python3 pyenv-virutalenvwrapper
 brew install tree ag watch
