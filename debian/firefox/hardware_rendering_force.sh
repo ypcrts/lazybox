358ddb172e0cfa274a975f6541566a61a8ab0a41
ypcrts <ypcrts@users.noreply.github.com>
2017-08-19 13:47:03 -0400

    change everything, finally

---
+++ debian/firefox/hardware_rendering_force.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# # Force firefox to use hardware rendering
+cp 90-firefox /etc/X11/Xsession.d/
--- debian/x-environment.sh
+++ debian/x-environment.sh
@@ -1,61 +1,36 @@
 #!/bin/sh
-# set -e
-# . ./init.sh
-# DIR=`pwd`
-# set -x
-# sudo apt-get install\
-  # x11-xserver-utils\
-  # chromium\
-  # xdg-utils\
-  # libnotify-bin\
-  # xinit\
-  # dmenu\
-  # rxvt-unicode-256color\
-  # stow\
-  # make\
-  # build-essential\
-  # fonts-croscore\
-  # xclip\
-  # xsel\
-  # xbacklight\
-  # xinput\
-  # suckless-tools\
-  # xautolock\
-  # i3lock/unstable\
-  # imagemagick\
-  # scrot\
-  # compton\
-  # feh\
-  # rofi/unstable\
-  # xdotool\
-  # iceweasel\
-  # vim-gtk\
-  # xfonts-terminus xfonts-terminus-oblique\
-  # ttf-mscorefonts-installer fonts-cantarell fonts-cabin fonts-ebgaramond fonts-ebgaramond-extra fonts-jura fonts-lato fonts-linuxlibertine fonts-oxygen fonts-powerline\
-  # redshift
+# target: debian stretch, jessie
+apt-get install\
+  x11-xserver-utils xinit xdg-utils xdotool\
+  policykit-1 xscreensaver \
+  lxappearance arandr qt4-qtconfig \
+  surfraw surfraw-extra \
+  bspwm sxhkd lemonbar \
+  dunst libnotify-bin\
+  dmenu\
+  rxvt-unicode-256color\
+  make\
+  build-essential\
+  fonts-croscore\
+  xsel xinput\
+  suckless-tools xautolock\
+  imagemagick\
+  scrot\
+  compton feh rofi\
+  firefox-esr chromium\
+  vim-gtk\
+  xfonts-terminus xfonts-terminus-oblique\
+  ttf-mscorefonts-installer fonts-cantarell fonts-cabin fonts-ebgaramond fonts-ebgaramond-extra fonts-jura fonts-lato fonts-linuxlibertine fonts-oxygen fonts-powerline\
+  redshift
 
+( ./x-environment-gtk-icons.sh )
 
-# #firefox-esr\
+( ./x-environment-gtk-theme.sh )
+
+( ./x-environment-siji-font.sh )
 
 # # Dependencies for sxhkd and bspwm
 # # https://github.com/windelicato/dotfiles/wiki/bspwm-for-dummies
-# sudo apt-get install xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev
+# apt-get install xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev
 
-# # Force firefox to use hardware rendering
-# sudo cp x-environment/90-firefox /etc/X11/Xsession.d/
-
-set +x
-
-cd "$PWD"
-./x-environment-bspwm.sh
-
-cd "$PWD"
-./x-environment-sxhkd.sh
-
-cd "$PWD"
-./x-environment-gtk-icons.sh
-cd "$PWD"
-./x-environment-gtk-theme.sh
-cd "$PWD"
-./x-environment-siji-font.sh
 
