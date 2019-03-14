3dbafe592d0eab4c678190ca55428f7fbd34bd03
ypcrts <32625281+ypcrts@users.noreply.github.com>
2019-03-14 13:31:08 -0400

    zardus yawn

--- tools.sh
+++ tools.sh
@@ -3,21 +3,8 @@ echo Clone repo...
 if [ -d ~/tools ]; then
   echo '~/tools exists, skipping'
 else
-  git clone gitlab:ypcrts/tools ~/tools
+  git clone github:ypcrts/tools ~/tools
 fi
-
-echo Symlink zardus management script
-mkdir -p ~/bin
-cd ~/bin
-
-TARGET=~/tools/tools.zardus/bin/manage-tools
-if [ ! -x "$TARGET" ]; then
-  echo "assertion failed: $TARGET missing +x or does not exist"
-  echo "skipping"
-else
-  ln -sf  "$TARGET" zardus
-fi
-
 
 echo ''
 echo "NB zardus depends on:  debian/apt.py --with-all"
