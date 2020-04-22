b63b83cfaa91189ec35acd9c8e6d3409214a62d4
ypcrts <32625281+ypcrts@users.noreply.github.com>
2020-04-21 22:11:48 -0400

    dots.sh python3 autodetectionnn lazy

--- dots.sh
+++ dots.sh
@@ -15,7 +15,13 @@ cd ~/Projects
 git clone https://github.com/ypcrts/dots || \
   die 'clone failed'
 cd dots
-python manifest.py -fv install  || \
+if command -V python 2>&1 >/dev/null;  then
+  PYBIN=python
+elif command -V python3 2>&1 >/dev/null;  then
+  PYBIN=python3
+fi
+
+$PYBIN manifest.py -fv install  || \
   die 'manifest install failed'
 
 printf '\nsuccess: dots.sh\n'
