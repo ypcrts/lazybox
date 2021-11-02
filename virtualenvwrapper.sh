1498bd1c1c6dd1133c40e0abd31059d5200cc476
ypcrts <32625281+ypcrts@users.noreply.github.com>
2021-11-02 22:13:38 +0000

    virtualenvwrapper.sh

---
+++ virtualenvwrapper.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+pip3 install virtualenvwrapper
+>>~/.config/bash/hosts/local.private.sh echo 'export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3'
+echo 'now source . ~/.local/bin/virtualenvwrapper.sh'
