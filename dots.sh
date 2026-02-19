75ea6e6487bbcba64cdccbc6ad0bfee976f8491e
ypcrts <32625281+ypcrts@users.noreply.github.com>
2026-02-18 19:52:27 -0500

    toda

--- dots.sh
+++ dots.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -xo 
+
 die () {
   1>&2 echo abort: "$@"
   exit 1
@@ -12,15 +14,26 @@ fi
 
 mkdir -p ~/Projects
 cd ~/Projects
+
 git clone https://github.com/ypcrts/dots || \
   die 'clone failed'
-cd dots
+
+git clone https://github.com/ypcrts/toda || \
+  die 'clone failed'
+
 if command -V python >/dev/null 2>&1;  then
   PYBIN=python
 elif command -V python3 >/dev/null 2>&1;  then
   PYBIN=python3
 fi
 
+cd toda
+$PYBIN -m pip install . || \
+  die 'toda install fail'
+
+cd ..
+
+cd dots
 $PYBIN manifest.py -fv install  || \
   die 'manifest install failed'
 
